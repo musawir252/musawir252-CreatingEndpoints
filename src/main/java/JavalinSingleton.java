c30c6a3a364784a0f454efc874bc75880d8591d5
musawir252 <musawir252@gmail.com>
2023-02-24 21:10:01 +0000

    Completed JavalinSingleton Lab

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -18,6 +18,12 @@ public class JavalinSingleton {
         Javalin app = Javalin.create();
         
         //write endpoint here
+        app.get("/hello", ctx -> {
+            //logic to be executed when this endpoint is hit
+            ctx.result("Hello World");
+        });
+
+       
 
         return app;
     }
